487086176a382bdeebd5fdd20667dfbc7b5044f6
Praveen <praveen.k@xnara.com>
2025-04-25 22:30:19 +0530

    added objects

---
+++ Employee.ts
@@ -0,0 +1,47 @@
+// class  - ES6 (ECMA SCRIPT 6 - 2015)
+// Syntax : 
+
+/*
+class ClassName{
+
+// propeties
+
+key1 : datatype
+key2 : datatype
+private key3
+
+static key4
+
+constructor(key, keys, keys1){
+   this.key1 = key
+   this.key2 = keys
+   this.key3 = keys1
+}
+
+// methods
+
+info(arg) :returnType{
+// code
+this.key1
+}
+
+static info1(arg) :returnType{
+ //code
+}
+
+}
+
+
+// ClassName obj = new ClassName(....) - Java
+
+let obj  = new ClassName(parameter)
+
+obj.key1
+obj.info()
+obj.key3
+
+ClassName.key4
+ClassName.info1()
+
+
+*/
--- objects.ts
+++ objects.ts
@@ -19,18 +19,19 @@
 // let or const  obj = {name: value, function: function}
 
 const person = {
-    name1 : "John",
+    name : "John",
     age : 30,
     isEmployed : true,
     personInfo : function(){
-        console.log(`name is :  ${person.name1}, age is ${this.age}, is Employed ${this.isEmployed}`)
+        console.log(`name is :  ${person.name}, age is ${this.age}, is Employed ${this.isEmployed}`)
     }
 }
 
 // this - Refers to an object
 
 // obj.key
-console.log(person.name1);
+person.name = "Rahul"
+console.log(person.name);
 console.log(person.age);
 person.personInfo()
 
@@ -38,19 +39,104 @@ person.personInfo()
 //console.log(person["personInfo"]());
 
 
+
+// 2. Inline Type Object - TS
+
+const person1 : {name1: string, age1: number, isEmployed: boolean, personInfo : Function} = 
+
+{
+    name1 : "John",
+    age1 : 30,
+    isEmployed : true,
+    personInfo : function(){
+        console.log(`name is :  ${this.name1}, age is ${this.age}, is Employed ${this.isEmployed}`)
+    }
+}
+
+console.log(person1.name1);
+console.log(person1.age1);
+person1.personInfo()
+
+// 3. Using Type aliases - TS
+
+// Syntax:
+// type variableName = {key:value}
+
+type human = {
+    name : string,
+    age: number,
+    city : string, 
+    person? : Function
+}
+
+
+let person2 : human = {name : "Jay", age : 34, city : "Delhi"}
+let person3 : human = {name : "Ajay", age : 30, city : "NY"}
+
+console.log(person2.name);
+console.log(person2.age);
+
+// 4. interface 
+// Syntax:
+// interface name {key:value}
+
+interface Employee {
+    name: string
+    age : number
+    role : string
+    salary?: number
+}
+
+interface Company extends Employee{
+    dept : string
+}
+
+let person4 : Employee = {name : "Ajay", age : 30, role : "SE", salary : 100000}
+let person5 : Company = {name : "Ajay", age : 30, role : "SE", salary : 100000, dept : "IT"}
+
+
+
+//6. Factory function - A function which will return an object is known as a factory function
+// Syntax:
+
+/*
+function functionName(agr){
+   // .....
+   return object
+}
+*/
+
+function Car(model, year, seat){
+    // .....
+    return {
+        model1: model,
+        year1 : year,
+        seat1 : seat
+    }
+}
+
+let car = Car("BMW", "i7", 5)
+console.log(car.model1);
+
+
+
+
+
+
+
 class Test{
     name1 
     age
     constructor(a, b) {
-        this.name1 = a
-        this.age = b
-        
+        this.name1 = a 
+        this.age = b 
     }
+
 }
 
 
 let test1 = new Test("Rahul", 80)
-console.log(test1)
+console.log(test1.name1)
 
 
 
@@ -59,3 +145,4 @@ console.log(test1)
 
 
 
+// 5. Class style
